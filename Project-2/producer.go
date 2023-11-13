2265d1141d745c04bc1892f0884565a0eda805ea
Gromovoi Stanislav <gromila12@rambler.ru>
2023-11-13 15:35:04 +0600

    Project-2

---
+++ Project-2/producer.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"context"
+	"crypto/tls"
+	"log"
+	"os"
+
+
+	"github.com/joho/godotenv"
+	"github.com/segmentio/kafka-go"
+	"github.com/segmentio/kafka-go/sasl/scram"
+)
+
+func main() {
+	// Загрузите переменные среды из файла .env
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
+	KAFKA_HOSTNAME := os.Getenv("KAFKA_HOSTNAME")
+	KAFKA_USERNAME := os.Getenv("KAFKA_USERNAME")
+	KAFKA_PASSWORD := os.Getenv("KAFKA_PASSWORD")
+
+	mechanism, err := scram.Mechanism(scram.SHA256, KAFKA_USERNAME, KAFKA_PASSWORD)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	
+	dialer := &kafka.Dialer{
+		SASLMechanism: mechanism,
+		TLS:           &tls.Config{},
+	}
+	
+
+	w := kafka.NewWriter(kafka.WriterConfig{
+		Brokers: []string{KAFKA_HOSTNAME},
+		Dialer:  dialer,
+	})
+	
+	err = w.WriteMessages(context.Background(),
+		kafka.Message{
+			Topic: "topic-A",
+			Key:   []byte("Key-A"),
+			Value: []byte("Hello World!"),
+		},
+		kafka.Message{
+			Topic: "topic-B",
+			Key:   []byte("Key-B"),
+			Value: []byte("One!"),
+		},
+		kafka.Message{
+			Topic: "topic-C",
+			Key:   []byte("Key-C"),
+			Value: []byte("Two!"),
+		},
+	)
+	if err != nil {
+		log.Fatal("failed to write messages:", err)
+	}
+
+	if err := w.Close(); err != nil {
+    	log.Fatal("failed to close writer:", err)
+	}
+}
